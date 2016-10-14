2e4c424fe079548d33c0e261fe38d447c09511a2
greenvity-kiran <kiran_nevaskar@greenvity.com>
2016-10-14 19:20:50 +0530

    GV701x Flash Tool to support Hi-Rel bootloader
    This tool is backward compatible with GV701x Flash Tool

---
+++ ASIC/GV701x_Flashing_Utility_vb -4_0_0_12_HI_REL_Boot/GV701x_Flashing_Utility_vb/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Flashing Utility")>
+<Assembly: AssemblyDescription("Flashing Utility")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Flashing Utility")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("096e5297-78b4-45b3-a13d-8df6571b364e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("4.0.0.12")>
+<Assembly: AssemblyFileVersion("4.0.0.12")>
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
